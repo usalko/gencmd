d6229c3fb27437ade9b9bf9552b98d95a86451ad
Ivan Usalko <i.usalko@itransition.com>
2021-01-06 15:36:13 +0300

    # GENCMD-1
    Initial implementation
     - Code cleaning

--- gencmd.h
+++ gencmd.h
@@ -11,37 +11,12 @@ typedef int32_t i32;
 typedef char* charptr;
 
 typedef struct string string;
-typedef struct array array;
-typedef struct Option_string Option_string;
 
 struct string {
 	byteptr str;
 	int len;
 	int is_lit;
 };
-
-struct array {
-	int element_size;
-	voidptr data;
-	int len;
-	int cap;
-};
-
-struct Option_string {
-	bool ok;
-	bool is_none;
-	string v_error;
-	int ecode;
-	byteptr data;
-};
-
-typedef struct gencmd__Blob {
-	byteptr data;
-	i32 length;
-	charptr errors;
-} gencmd__Blob;
-
-typedef array array_byte;
 
 extern string gencmd__gencmd(string cmd);
 
